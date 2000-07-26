de4d69bb9ce14ee77fef543fd443aa9d590d7e8e
asl <asl@ecbc3a18-8f1b-0410-a2db-9d670ffdbd2b>
2000-07-26 16:13:21 +0000

    This commit was manufactured by cvs2svn to create branch 'start'.

---
+++ branches/start/src/includes.h
@@ -0,0 +1,46 @@
+#ifndef __HLFL_INCLUDES__
+#include "config.h"
+#ifdef HAVE_STDIO_H
+#include <stdio.h>
+#endif
+
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+
+#ifdef HAVE_STRING_H
+#include <string.h>
+#endif
+
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif
+
+#ifdef HAVE_SYS_SOCKET_H
+#include <sys/socket.h>
+#endif
+
+#ifdef HAVE_NETINET_IN_H
+#include <netinet/in.h>
+#endif
+
+#ifdef HAVE_ARPA_INET_H
+#include <arpa/inet.h>
+#endif
+
+
+#define ACCEPT_ONE_WAY 1			/*  ->           */
+#define ACCEPT_ONE_WAY_REVERSE 2		/* <-            */
+#define ACCEPT_TWO_WAYS 3 			/* <->           */
+#define ACCEPT_TWO_WAYS_ESTABLISHED 4 		/* <=>> 	 */
+#define ACCEPT_TWO_WAYS_ESTABLISHED_REVERSE 11  /* <<=>		 */
+#define DENY 5					/*  X            */
+#define REJECT 6				/*  X!           */
+#define DENY_OUT 7				/* X->           */
+#define DENY_IN 8				/* <-X           */
+#define REJECT_OUT 9				/* X!->          */
+#define REJECT_IN 10				/* <-X!          */
+
+#define COMMENT 1
+#define INCLUDE_TEXT 2
+#endif
