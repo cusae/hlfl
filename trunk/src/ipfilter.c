4fa2fb5510d3f2955f95a85f784ee06253662084
asl <asl@ecbc3a18-8f1b-0410-a2db-9d670ffdbd2b>
2001-08-05 14:10:32 +0000

    Correction of X! behaviour following Sven Huster <sven.huster@mailsurf.com> suggestion.

--- trunk/src/ipfilter.c
+++ trunk/src/ipfilter.c
@@ -185,12 +185,12 @@ translate_ipfilter(op, proto, src, log, dst, sports, dports, interface)
 	   dst, dports, src, sports, icmp_code);
     break;
    case REJECT_ALL:
-    printf("block in%s quick %s %s from %s %s to %s %s %s\n", logit, via, p,
-	   src, sports, dst, dports, icmp_code);
-    printf("block out%s quick %s %s from %s %s to %s %s %s\n", logit, via, p,
-	   src, sports, dst, dports, icmp_code);
     printf("block return-icmp in%s quick %s %s from %s %s to %s %s %s\n", logit,
 	   via, p, dst, dports, src, sports, icmp_code);
+    printf("block in%s quick %s %s from %s %s to %s %s %s\n", logit, via, p,
+	   dst, dports, src, sports, icmp_code);
+    printf("block out%s quick %s %s from %s %s to %s %s %s\n", logit, via, p,
+	   src, sports, dst, dports, icmp_code);
     break;
    case DENY_OUT:
     printf("block out%s quick %s %s from %s %s to %s %s %s\n", logit, via, p,
