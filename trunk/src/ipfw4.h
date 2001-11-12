dbe512a149fd2911eda4595dccf8199aa5465a72
asl <asl@ecbc3a18-8f1b-0410-a2db-9d670ffdbd2b>
2001-11-12 17:18:22 +0000

    Preliminary support for ipfw statefull

---
+++ trunk/src/ipfw4.h
@@ -0,0 +1,8 @@
+#ifndef HLFL__IPFW4_H
+#define HLFL__IPFW4_H
+
+int translate_ipfw4(int, char *, char *, int, char *, char *, char *, char *);
+int translate_ipfw4_start();
+void include_text_ipfw4();
+
+#endif				/* HLFL__IPFW4_H */
