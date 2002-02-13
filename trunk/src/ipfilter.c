d3270823d48e6675fd6827c298fe2cb37da6d64e
asl <asl@ecbc3a18-8f1b-0410-a2db-9d670ffdbd2b>
2002-02-13 10:35:09 +0000

    TCP uses RST to indicate a connection refused. (cf. Stevens, vol1, p. 247) Reported by Adrien Huvier <nabok@nabok.org>

--- trunk/src/ipfilter.c
+++ trunk/src/ipfilter.c
@@ -185,8 +185,12 @@ translate_ipfilter(op, proto, src, log, dst, sports, dports, interface)
 	   dst, dports, src, sports, icmp_code);
     break;
    case REJECT_ALL:
-    printf("block return-icmp in%s quick %s %s from %s %s to %s %s %s\n", logit,
+    if (!strcmp(proto, "tcp"))
+    printf("block return-rst in%s quick %s %s from %s %s to %s %s %s\n", logit,
 	   via, p, dst, dports, src, sports, icmp_code);
+    else
+    printf("block return-icmp in%s quick %s %s from %s %s to %s %s %s\n", logit,
+	    via, p, dst, dports, src, sports, icmp_code);
     printf("block out%s quick %s %s from %s %s to %s %s %s\n", logit, via, p,
 	   src, sports, dst, dports, icmp_code);
     break;
@@ -203,6 +207,10 @@ translate_ipfilter(op, proto, src, log, dst, sports, dports, interface)
 	   src, sports, dst, dports, icmp_code);
     break;
    case REJECT_IN:
+    if(!strcmp(proto, "tcp"))
+    printf("block return-rst in%s quick %s %s from %s %s to %s %s %s\n", logit,
+	   via, p, dst, dports, src, sports, icmp_code);
+    else
     printf("block return-icmp in%s quick %s %s from %s %s to %s %s %s\n", logit,
 	   via, p, dst, dports, src, sports, icmp_code);
     break;
