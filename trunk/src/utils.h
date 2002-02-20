6ea7f9848826de72fbb688a0ba87dcdd22ab8088
asl <asl@ecbc3a18-8f1b-0410-a2db-9d670ffdbd2b>
2002-02-20 14:07:21 +0000

    Support for Solaris, should compile and work.

--- trunk/src/utils.h
+++ trunk/src/utils.h
@@ -4,4 +4,8 @@
 char *matching_items(char *, char, char);
 char *strchr_items(char *, char, char, char);
 
+#ifndef HAVE_INET_ATON
+int inet_aton(register const char *, struct in_addr *);
+#endif				/* ! HAVE_INET_ATON */
+
 #endif				/* HLFL_UTILS_H */
