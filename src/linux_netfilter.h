4916263f0f8a7af9d5af7c3648b85b941d3b232e
Arnaud Launay <alaunay@users.noreply.github.com>
2002-10-08 10:16:04 +0000

    - iptables was not working, first set of patches from Carlos Villegas <villegas@math.gatech.edu> - corrects the rules and the log stuff

--- src/linux_netfilter.h
+++ src/linux_netfilter.h
@@ -1,6 +1,10 @@
 #ifndef HLFL_LINUX_netfilter_H__
 #define HLFL_LINUX_netfilter_H__
 
+/* Add logging stuff while I (Carlos Villegas) am working in iptables support */
+#define HLFL_LINUX_netfilter_LOG_LEVEL info
+#define HLFL_LINUX_netfilter_LOG_PREFIX iptables
+
 int translate_linux_netfilter(int, char *, char *, int, char *, char *, char *,
 			      char *);
 int translate_linux_netfilter_start(FILE *);
